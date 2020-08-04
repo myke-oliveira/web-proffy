ce5222de9f6603861abebef4a5b7452f768876f3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-03 23:34:42 -0300

    wip: layout

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,24 +4,25 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg'
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './style.css'
+
 interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
-  return (
-    <header className="page-header" >
-      <div className="top-bar-container">
-        <Link to="/">
-          <img src={backIcon} alt="Voltar" />
-        </Link>
-        <img src={logoImg} alt="Proffy" />
-      </div>
-      <div className="header-content">
-        <strong>{title}</strong>
-      </div>
-    </header >
-  );
-}
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children }) => (
+  <header className="page-header">
+    <div className="top-bar-container">
+      <Link to="/">
+        <img src={backIcon} alt="Voltar" />
+      </Link>
+      <img src={logoImg} alt="Proffy" />
+    </div>
+    <div className="header-content">
+      <strong>{title}</strong>
+      {children}
+    </div>
+  </header>
+)
 
 export default PageHeader;
