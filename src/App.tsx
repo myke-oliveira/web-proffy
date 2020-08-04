ee4c88a04c3089e7763968027217418754c3dd7d
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-03 22:08:09 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import Landing from './pages/Landing';
-
 import './assets/styles/global.css';
+import Routes from './Routes';
 
 function App() {
-
   return (
-    <Landing/>
+    <Routes />
   ); 
 }
 
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg'
@@ -18,14 +19,14 @@ function Landing() {
         </div>
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
         <div className="buttons-container">
-          <a href="https://www.facebook.com.br" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="https://www.facebook.com.br" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
