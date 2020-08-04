b9b23dc56cc1c02ece982fe61b31e8f2e1860538
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-03 22:46:42 -0300

    header component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
-import TeacherList from './pages/TeacherLIst';
-import TeacherForm from './pages/TeacherForm/TeacherForm';
+import TeacherList from './pages/TeacherList';
+import TeacherForm from './pages/TeacherForm';
 
 function Routes() {
   return (
