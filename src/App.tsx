f2fec2efb5f76c4250775c94379ac2702b6367f5
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-08-03 21:47:55 -0300

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello World!!!</h1>
-    </div>
-  );
+    <Landing/>
+  ); 
 }
 
 export default App;
